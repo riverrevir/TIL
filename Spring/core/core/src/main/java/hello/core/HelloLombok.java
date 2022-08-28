3e598b60d803f8fef2dd92e301701cfb3e5074d8
riverrevir <81510864+riverrevir@users.noreply.github.com>
2022-08-29 08:26:37 +0900

    feat: 애노테이션 만들어보기

--- Spring/core/core/src/main/java/hello/core/HelloLombok.java
+++ Spring/core/core/src/main/java/hello/core/HelloLombok.java
@@ -9,12 +9,12 @@ public class HelloLombok {
     private String name;
     private int age;
 
-    public static void main(String[] args) {
-       HelloLombok helloLombok=new HelloLombok();
-       helloLombok.setName("asdf");
-       helloLombok.setAge(1);
-
-       String name= helloLombok.getName();
-       System.out.println("name = " + name);
-    }
+//    public static void main(String[] args) {
+//       HelloLombok helloLombok=new HelloLombok();
+//       helloLombok.setName("asdf");
+//       helloLombok.setAge(1);
+//
+//       String name= helloLombok.getName();
+//       System.out.println("name = " + name);
+//    }
 }
